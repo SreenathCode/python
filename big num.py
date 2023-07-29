515763fb423834f1a13b3dd564e121b2445fbac7
Sreenath <140224929+SreenathCode@users.noreply.github.com>
2023-07-29 17:10:14 +0530

    big number

---
+++ big num.py
@@ -0,0 +1,5 @@
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+result = find_bigger_number(number1, number2)
+print("The bigger number is:", result)
